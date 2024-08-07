d32ee95de347b36c33a0dba717f462a7ed5d4b08
kenkaigu <31952302+kenkaigu@users.noreply.github.com>
2024-08-07 19:05:34 +0300

    Create config.php
    testing a different file format

---
+++ storage-service/src/main/resources/config.php
@@ -0,0 +1,17 @@
+<?
+define( 'DB_NAME', 'itu_test' );
+
+/** Database username */
+define( 'DB_USER', 'root' );
+
+/** Database password */
+define( 'DB_PASSWORD', '' );
+
+/** Database hostname */
+define( 'DB_HOST', 'localhost' );
+
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
